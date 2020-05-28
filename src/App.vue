0e16699bb38c7f92e0efd95d347b5e847b792366
Jon <jconlin@ufl.edu>
2020-05-28 16:47:28 -0400

    initialized component for creating new user posts

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Post/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Post from './components/Post.vue';
+//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Post,
+   // HelloWorld
   }
 }
 </script>
